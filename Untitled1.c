d6e5a8517e32d933bb1ee69424786410dc3a2390
KavyaShahh07 <deadpoool123451@gmail.com>
2024-07-31 12:35:41 +0530

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    
+   // printf() displays the string inside quotation
+   printf("My Name is Kavya Shah");
+   printf("\n deadpoool123451@gmail.com");
+   printf("\n age is 17");
+   printf("\n add opp mun mar cg road");
+   printf("\n 1234567890");
+   return 0;
+}
